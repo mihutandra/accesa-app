4f2097556bbd4d13d5221584e0b89863754c6b7e
mihutandra <andra.mihut@stud.ubbcluj.ro>
2025-05-11 20:23:12 +0300

    initial commit

---
+++ src/main/java/com/andra/accesaapp/AccesaAppApplication.java
@@ -0,0 +1,13 @@
+package com.andra.accesaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andra/accesaapp/AccesaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andra.accesaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
